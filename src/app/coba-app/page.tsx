eb4f730fe8996fb0842aeab857fa33b706f221b1
Andi Setiawan (andi@code.id) <andi@code.id>
2024-09-13 11:02:25 +0700

    coba pakai generator metadata

--- src/app/coba-app/page.tsx
+++ src/app/coba-app/page.tsx
@@ -10,31 +10,34 @@ export async function generateMetadata({
   searchParams,
 }: Props): Promise<Metadata> {
   const id = searchParams.id;
+  console.log(id);
 
-  const anime = await fetch(`https://api.jikan.moe/v4/anime/${id}`)
-    .then((res) => res.json())
-    .then((response) => response.data);
+  // const anime = await fetch(`https://api.jikan.moe/v4/anime/${id}`)
+  //   .then((res) => res.json())
+  //   .then((response) => response.data);
 
   const CustomMetaData = {
-    title: anime?.title_english,
-    description: anime?.synopsis,
+    title: "ini adalah judul",
+    description: "ini deskripsi",
     openGraph: {
-      title: anime?.title_english || "ini adalah judul",
-      description: anime?.synopsis || "ini deskripsi",
+      title: "ini adalah judul",
+      description: "ini deskripsi",
       type: "website",
       siteName: "coba doang",
       url: "https://awandri.com",
       images: [
         {
-          url: anime?.images?.jpg?.image_url,
+          url: "https://og.awandri.com/api/general?bgImageUrl=https%3A%2F%2Fcdn.myanimelist.net%2Fimages%2Fanime%2F1439%2F93480.jpg&bgType=image&description=Gabung%20bersama%20kami%21&siteName=PRUAFFILIATE&title=COBA%20COBA%20DOANG",
         },
       ],
     },
     twitter: {
       card: "summary_large_image",
-      title: anime?.title_english || "ini adalah judul",
-      description: anime?.synopsis || "ini deskripsi",
-      images: [anime?.images?.jpg?.image_url],
+      title: "ini adalah judul",
+      description: "ini deskripsi",
+      images: [
+        "https://og.awandri.com/api/general?bgImageUrl=https%3A%2F%2Fcdn.myanimelist.net%2Fimages%2Fanime%2F1439%2F93480.jpg&bgType=image&description=Gabung%20bersama%20kami%21&siteName=PRUAFFILIATE&title=COBA%20COBA%20DOANG",
+      ],
     },
   };
 
